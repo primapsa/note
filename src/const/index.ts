7ac571121c84229762f27e0dac828830533d9c2e
Siarhei Prymakovich <prima.psa@gmail.com>
2023-12-20 19:06:07 +0300

    feat: consts

---
+++ src/const/index.ts
@@ -0,0 +1,15 @@
+export const STATUS = {
+  ERROR: 'error',
+  IDLE: 'idle',
+  LOADING: 'loading',
+} as const
+
+export const MODE = {
+  EDIT: 'edit',
+  FORM: 'form',
+  LABEL: 'label',
+} as const
+
+export const TEXTAREA = {
+  SIZE: 3,
+}

28fd46f87f420e17523687c2b47c443b183509e6
Siarhei Prymakovich <prima.psa@gmail.com>
2023-12-20 19:05:29 +0300

    feat: add note label

--- src/components/note-label/note-label.tsx
+++ src/components/note-label/note-label.tsx
@@ -1,26 +1,26 @@
-import { CSSProperties, ComponentPropsWithoutRef } from 'react'
+import React, { CSSProperties, ComponentPropsWithoutRef } from 'react'
 
 import Typography from '@mui/material/Typography'
 
 import s from './note-label.module.scss'
 
-export const NoteLabel = ({
-  color,
-  onVisible,
-  ...props
-}: NoteLabelProps & Omit<ComponentPropsWithoutRef<'div'>, keyof NoteLabelProps>) => {
-  const toggleVisdible = () => onVisible(true)
-
-  return (
-    <div className={s['button']} onClick={toggleVisdible} {...props}>
-      <Typography color={color} variant="body2">
-        Take a note...
-      </Typography>
-    </div>
-  )
-}
+export const NoteLabel = React.memo(
+  ({
+    color,
+    onLabelClick,
+    ...props
+  }: NoteLabelProps & Omit<ComponentPropsWithoutRef<'div'>, keyof NoteLabelProps>) => {
+    return (
+      <div className={s['button']} onClick={onLabelClick} {...props}>
+        <Typography color={color} variant="body2">
+          Take a note...
+        </Typography>
+      </div>
+    )
+  }
+)
 
 type NoteLabelProps = {
   color?: CSSProperties['color']
-  onVisible: (v: boolean) => void
+  onLabelClick: () => void
 }

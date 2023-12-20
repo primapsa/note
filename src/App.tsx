f6c0f4baf9245bab08ea44afb63717e7e9fcae46
Siarhei Prymakovich <prima.psa@gmail.com>
2023-12-20 19:13:34 +0300

    feat: add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import Box from '@mui/material/Box'
 
-const App = () => {
-  return <div>Notes</div>
+import s from './app.module.scss'
+
+import { AddNote } from './components/addnote'
+import { Header } from './components/header'
+import { Edit } from './container/edit'
+import { Filter } from './container/filter'
+import { Notes } from './container/notes'
+export const App = () => {
+  return (
+    <Box className={s['container']}>
+      <Header />
+      <Box className={s['manage']}>
+        <AddNote />
+        <Filter />
+      </Box>
+      <Notes />
+      <Edit />
+    </Box>
+  )
 }
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import 'styles/index.scss'
 
+import { App } from './App'
+import reportWebVitals from './reportWebVitals'
+import { store } from './store'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
 
@@ -17,3 +22,26 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+/*
+Разработать одностраничное приложение на React.js (SPA): текстовый редактор заметок с тегами.
+
+Функционал:
+1. Создание, редактирование, удаление заметок;
+2. Просмотр списка заметок;
+3. Во время создания и редактирования заметки пользователь может создавать теги,
+используя символ # (Например, пользователь вводит текст “I wanna go to #shop tomorrow”.
+По мере ввода должен создаться соответствующий тег и отобразиться в списке под текстовым полем.
+ При редактировании заметки все слова, соответствующие тегам, должны подсвечиваться, т.e. “I wanna go to #shop tomorrow”);
+4. Фильтр заметок по тегу - пользователь должен иметь возможность выбрать один или несколько тегов из существующих и по ним отфильтровать список заметок;
+5. При перезагрузке страницы заметки должны сохраняться (Например, хранить в IndexedDb).
+
+Технические требования:
+1. React.js hooks;
+2. Использование TypeScript;
+3. Использование UI библиотек (Например, Material UI, Ant design);
+4. Использовать стейт менеджер (Например, Redux, MobX, прочее);
+5. Залить на гит (Публичный репозиторий);
+6. Залить на хост (Netlify, Github pages, прочее).
+
+ */
